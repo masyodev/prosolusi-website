59c7eeab436048041dd5a137f97fb6570b1d63d1
masyodev <ydevitra@gmail.com>
2024-06-14 20:57:28 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Contoh animasi scroll
-$(document).ready(function() {
-    $('.cta-button').on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500);
-    });
-});
